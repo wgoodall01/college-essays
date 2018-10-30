f086bced4020b53167fba9c18e1f961242186dbd
William Goodall <wgoodall01@gmail.com>
2018-10-30 18:18:58 -0400

    <Shade/>: custom container element

--- src/components/Shade.js
+++ src/components/Shade.js
@@ -2,10 +2,13 @@ import React from 'react';
 import classnames from 'classnames';
 import './Shade.css';
 
-const Shade = ({children, className, ...rest}) => (
-  <span className={classnames('Shade', className)} {...rest}>
-    {children}
-  </span>
-);
+const Shade = ({children, className, tag, ...rest}) => {
+  const Container = 'span' || tag;
+  return (
+    <Container className={classnames('Shade', className)} {...rest}>
+      {children}
+    </Container>
+  );
+};
 
 export default Shade;
